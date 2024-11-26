0bcbd6b4b34f7dbb1130100e5f3c5adf93c2bb41
keyinora <ddraven1989@gmail.com>
2024-11-26 11:39:16 -0500

    update a few courses

--- polynomial_time/sorting/merge_sort/main.py
+++ polynomial_time/sorting/merge_sort/main.py
@@ -1,33 +1,26 @@
 def merge_sort(nums):
-    # Base case: If the list has 1 or no elements, it's already sorted
-    if len(nums) <= 1:
+    if len(nums) < 2:
         return nums
-    
-    # Find the midpoint and split the list into two halves
-    mid = len(nums) // 2
-    sorted_left_side = merge_sort(nums[:mid])
-    sorted_right_side = merge_sort(nums[mid:])
-    
-    # Merge the sorted halves
-    return merge(sorted_left_side, sorted_right_side)
+    first = merge_sort(nums[: len(nums) // 2])
+    second = merge_sort(nums[len(nums) // 2 :])
+    return merge(first, second)
 
 
 def merge(first, second):
     final = []
     i = 0
     j = 0
-    
-    # Continue until one of the halves is exhausted
     while i < len(first) and j < len(second):
-        if first[i] < second[j]:
+        if first[i] <= second[j]:
             final.append(first[i])
             i += 1
         else:
             final.append(second[j])
             j += 1
-    
-    # Append any remaining elements
-    final.extend(first[i:])
-    final.extend(second[j:])
-    
+    while i < len(first):
+        final.append(first[i])
+        i += 1
+    while j < len(second):
+        final.append(second[j])
+        j += 1
     return final
